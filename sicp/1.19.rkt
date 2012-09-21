928b9e0a7d5a3216fba0ffb6d759b98076a42afd
Zhang Shaoyu <zsy056@gmail.com>
2012-09-21 23:57:03 +0800

    sicp 1.19

---
+++ sicp/1.19.rkt
@@ -0,0 +1,25 @@
+#! /usr/bin/env racket
+#lang racket
+
+(define (fib n)
+  (fib-iter 1 0 0 1 n))
+(define (fib-iter a b p q count)
+  (cond ((= count 0) b)
+        ((even? count)
+         (fib-iter a
+                   b
+                   (+ (* p p) (* q q))
+                   (+ (* 2 p q) (* q q))
+                   (/ count 2)))
+        (else (fib-iter (+ (* b q) (* a q) (* a p))
+                        (+ (* b p) (* a q))
+                        p
+                        q
+                        (- count 1)))))
+
+(fib 1)
+(fib 2)
+(fib 3)
+(fib 10)
+(fib 11)
+(fib 12)
