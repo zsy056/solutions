5b9d63b391b77a709b88136e61e8157434811963
Shaoyu Zhang <zsy056@gmail.com>
2014-11-23 21:52:47 +0900

    pat 1025, haskell solution still TLE on the last case

--- pat/1025.hs
+++ pat/1025.hs
@@ -1,14 +1,6 @@
 -- TLE on last case
 import qualified Data.List as List
-
-merge :: (Ord a) => [a] -> [a] -> [a]
-merge [] [] = []
-merge ox@(x:xs) [] = ox
-merge [] ox@(x:xs) = ox
-merge ox@(x:xs) oy@(y:ys)
-    | x < y  = x : merge xs oy
-    | x > y  = y : merge ox ys
-    | x == y = x : y : merge xs ys 
+import qualified Data.Sequence as Seq
 
 putAll :: (Show a) => [a] -> IO ()
 putAll [] = return ()
@@ -50,8 +42,8 @@ instance Eq Student where
     (Student a _ _ _ _) == (Student b _ _ _ _) = a == b
 
 instance Ord Student where
-    compare (Student regNum1 score1 _ _ _) (Student regNum2 score2 _ _ _)
-        = compare (score2, regNum1) (score1, regNum2)
+    compare (Student regNum1 _ _ _ _) (Student regNum2 _ _ _ _)
+        = compare regNum1 regNum2
 
 instance Show Student where
     show (Student regNum score locationNum localRank finalRank)
@@ -66,25 +58,59 @@ updateLocalRank stu rank = stu { localRank = rank }
 updateFinalRank :: Student -> Int -> Student
 updateFinalRank stu rank = stu { finalRank = rank }
 
-assignRank :: (Student -> Int -> Student) -> [Student] -> ([Student], Int)
-assignRank _ [] = ([], 0)
-assignRank rankUpdater lst@(s:ss) = doAssignRank lst (score s) rankUpdater 1 0
+localScores :: [Student] -> Seq.Seq [Student]
+localScores ss = doLocalScores ss (Seq.fromList [[] | i <- [0..100]])
 
-doAssignRank :: [Student] -> Int -> (Student -> Int -> Student) -> Int -> Int -> ([Student], Int)
-doAssignRank [] _ _ rank _ = ([], rank)
-doAssignRank (s:ss) lastScore rankUpdater rank same
-    | score s == lastScore = (rankUpdater s rank : fst res2, snd res2)
-    | otherwise = (rankUpdater s (rank + same) : fst res1, snd res1)
-    where res1 = doAssignRank ss (score s) rankUpdater (rank + same) 1
-          res2 = doAssignRank ss lastScore rankUpdater rank (same + 1)
+doLocalScores :: [Student] -> Seq.Seq [Student] -> Seq.Seq [Student]
+doLocalScores [] acc = acc
+doLocalScores (s:ss) acc = 
+    doLocalScores ss (Seq.update sScore (s:sameScore) acc)
+    where sScore = (score s)
+          sameScore = Seq.index acc sScore
 
-globalList :: [[Student]] -> [Student]
-globalList localLists 
-    = foldl merge [] (map (fst . (assignRank updateLocalRank) . List.sort) localLists)
+globalLists :: [Seq.Seq [Student]] -> Seq.Seq [Student]
+globalLists ss = doGlobalLists ss (Seq.fromList [[] | i <- [0..100]])
+
+doGlobalLists :: [Seq.Seq [Student]] -> Seq.Seq [Student] -> Seq.Seq [Student]
+doGlobalLists [] acc = acc
+doGlobalLists (s:ss) acc = doGlobalLists ss (mergeScore s acc)
+
+mergeScore :: Seq.Seq [Student] -> Seq.Seq [Student] -> Seq.Seq [Student]
+mergeScore a b = doMergeScore a b b 100
+
+doMergeScore :: Seq.Seq [Student] -> Seq.Seq [Student] -> Seq.Seq [Student] -> Int -> Seq.Seq [Student]
+doMergeScore a b acc (-1) = acc
+doMergeScore a b acc idx =
+    doMergeScore a b (Seq.update idx (as ++ bs) acc) (idx-1)
+    where as = Seq.index a idx
+          bs = Seq.index b idx 
+
+updateRank :: Seq.Seq [Student] -> (Student -> Int -> Student) -> (Seq.Seq [Student], Int)
+updateRank ss updater = foldl (updateRankIter updater) (ss, 1) [100,99..0]
+
+updateRankIter :: (Student -> Int -> Student) -> (Seq.Seq [Student], Int) -> Int -> (Seq.Seq [Student], Int)
+updateRankIter updater (ss, currentRank) score =
+    (Seq.update score afterUpdate ss, currentRank + sameScore)
+    where students = Seq.index ss score
+          sameScore = length students
+          afterUpdate = doUpdateRank students updater currentRank 
+
+doUpdateRank :: [Student] -> (Student -> Int -> Student) -> Int -> [Student]
+doUpdateRank ss updater newRank = map ((flip updater) newRank) ss
+
+outPut :: Seq.Seq [Student] -> IO ()
+outPut scores = doOutPut scores 100 
+
+doOutPut :: Seq.Seq [Student] -> Int -> IO ()
+doOutPut scores (-1) = return ()
+doOutPut scores score = do putAll $ List.sort (Seq.index scores score)
+                           doOutPut scores (score-1)
 
 main = do numCases <- getNumCases  
           locationLists <- getLocalLists numCases 
-          let final = assignRank updateFinalRank (globalList locationLists)
-          print $ snd final 
-          putAll $ fst final 
+          let locationScores = map (fst . ((flip updateRank) updateLocalRank)) (map localScores locationLists)
+          let globalScores = globalLists locationScores   
+          let (finalScores, totalStudents) = updateRank globalScores updateFinalRank
+          print $ totalStudents - 1 
+          outPut finalScores   
 
