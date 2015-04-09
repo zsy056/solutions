de5e3252df7f5b69d5f97c2443f76088a0bb53c5
Shaoyu Zhang <zsy056@gmail.com>
2015-04-09 22:04:21 +0800

    leetcode 153

---
+++ leetcode/153.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int> &num) {
+        return findMin(num, 0, num.size()-1);
+    }
+    int findMin(const vector<int> &num, int first, int last) {
+        if (first == last) {
+            return num[first];
+        }
+        if (first == last - 1) {
+            return min(num[first], num[last]);
+        }
+        if (num[first] < num[last]) {
+            return num[first];
+        }
+        auto mid = first + (last - first + 1) /2;
+        if (num[first] > num[mid]) {
+            return findMin(num, first, mid);
+        } else {
+            return findMin(num, mid, last);
+        }
+    }
+};
