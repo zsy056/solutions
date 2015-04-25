b8f3ac38d907355c09100b84de70d80c033fea3e
Shaoyu Zhang <zsy056@gmail.com>
2015-04-25 13:16:28 +0800

    leetcode 203

---
+++ leetcode/203.cpp
@@ -0,0 +1,35 @@
+/*
+Remove all elements from a linked list of integers that have value val.
+
+Example
+Given: 1 --> 2 --> 6 --> 3 --> 4 --> 5 --> 6, val = 6
+Return: 1 --> 2 --> 3 --> 4 --> 5
+
+Credits:
+Special thanks to @mithmatt for adding this problem and creating all test cases.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode dummy(0);
+        dummy.next = head;
+        auto current(&dummy);
+        while (current != nullptr) {
+            while (current->next != nullptr && current->next->val == val) {
+                current->next = current->next->next;
+                // delete current->next ?
+            }
+            current = current->next;
+        }
+        return dummy.next;
+    }
+};
