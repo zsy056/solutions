f813048f6c43077fdc48d8c16956e8f2d23efa62
Shaoyu Zhang <zsy056@gmail.com>
2015-06-02 18:06:44 +0800

    leetcode 48

---
+++ leetcode/48.cpp
@@ -0,0 +1,24 @@
+/*
+You are given an n x n 2D matrix representing an image.
+
+Rotate the image by 90 degrees (clockwise).
+
+Follow up:
+Could you do this in-place?
+*/
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i=0; i<n/2; ++i) {
+            for (int j=i; j<n-1-i; ++j) {
+                auto tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = tmp;
+            }
+        }
+    }
+};
