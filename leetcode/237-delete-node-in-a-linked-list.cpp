3992ce34b3fd9af92029d0d742a20701bf442f9a
Shaoyu Zhang <zsy056@gmail.com>
2015-09-18 09:33:22 +0800

    leetcode 237

--- leetcode/237-delete-node-in-a-linked-list.cpp
+++ leetcode/237-delete-node-in-a-linked-list.cpp
@@ -2,7 +2,6 @@
 Write a function to delete a node (except the tail) in a singly linked list, given only access to that node.
 
 Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node with value 3, the linked list should become 1 -> 2 -> 4 after calling your function.
-*
 */
 
 /**
@@ -16,9 +15,6 @@ Supposed the linked list is 1 -> 2 -> 3 -> 4 and you are given the third node wi
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        if (node == nullptr || node->next == nullptr) {
-            return;
-        }
         node->val = node->next->val;
         node->next = node->next->next;
     }
