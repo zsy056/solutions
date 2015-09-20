1b77d5a4d5e7158f57cc8096a1abf24e890dd759
Shaoyu Zhang <zsy056@gmail.com>
2015-09-20 20:58:48 +0800

    leetcode 14

--- leetcode/14-longest-common-prefix.rb
+++ leetcode/14-longest-common-prefix.rb
@@ -4,22 +4,18 @@ def longest_common_prefix(strs)
   if strs.empty?
     ''
   else
-    p = strs[0].split ''
-    p = strs[1..-1].inject(p) {|cp, b| lcp cp, b.split('') }
-    p.join('')
+    p = strs[0]
+    strs[1..-1].inject(p) {|cp, b| lcp cp, b }
   end
 end
 
 def lcp(a, b)
   i = 0
-  l = []
   while i < a.size and i < b.size
-    if a[i] == b[i]
-      l.push a[i]
-    else
+    if a[i] != b[i]
       break
     end
     i += 1
   end
-  l
+  a[0...i]
 end
