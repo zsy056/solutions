f163b62086756a5420b72b5fe9554718386b982b
Shaoyu Zhang <zsy056@gmail.com>
2015-10-19 22:49:59 +0800

    leetcode 139

--- leetcode/139-word-break.rb
+++ leetcode/139-word-break.rb
@@ -2,5 +2,15 @@
 # @param {Set<String>} word_dict
 # @return {Boolean}
 def word_break(s, word_dict)
-    
+  cache = Hash.new
+  _word_break s, word_dict, cache
+end
+
+
+def _word_break(s, word_dict, cache)
+  return cache[s] if cache.include? s
+  s.size.downto(1).each do |i|
+    return cache[s] = true if word_dict.include?(s[0...i]) and (i == s.size or _word_break(s[i..-1], word_dict, cache))
+  end
+  cache[s] = false
 end
